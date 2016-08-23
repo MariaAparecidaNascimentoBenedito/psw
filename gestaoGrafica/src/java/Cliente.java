69b8b2efce43820b9bd8751968c735820dd4e35a
Maria Aparecida do Nascimento Benedito Batista <RA21507734@uniceub.edu.br>
2016-08-23 18:12:16 -0300

    projeto test

---
+++ gestaoGrafica/src/java/Cliente.java
@@ -0,0 +1,38 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RA21507734
+ */
+@Entity
+public class Cliente {
+    
+    @Id
+    private String cpf;
+    private String nome;
+    
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
